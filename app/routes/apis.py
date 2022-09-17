ae050e8f0750d3a9a266272b524e7ab587b6e02c
Ahmed <eleliemy.ahmedhamdy@gmail.com>
2022-09-17 07:12:37 +0200

    adding / end point that return readme as html

---
+++ app/routes/apis.py
@@ -0,0 +1,11 @@
+import markdown
+
+#default will render the readme file
+@apis.route('/')
+def index():
+    #application_root_directory= os.path.abspath(os.path.dirname(__file__))
+    with open(application_root_directory+"/README.md", 'r') as home_page:
+        # read content of the file
+        content=home_page.read()
+        #convert markdown into html and return it
+        return markdown.markdown(content)
